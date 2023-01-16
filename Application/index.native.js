b6ce95d469e6472a571693f4eb00ac15877576b6
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-01-16 22:25:02 +0100

    Add react-native-navigation basic interface

--- Application/index.native.js
+++ Application/index.native.js
@@ -1,5 +1,19 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './src/app.json';
+import { Navigation } from "react-native-navigation";
+import App from "./src/App";
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('HomeScreen', () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+   Navigation.setRoot({
+     root: {
+       stack: {
+         children: [
+           {
+             component: {
+               name: 'HomeScreen'
+             }
+           }
+         ]
+       }
+     }
+  });
+});
