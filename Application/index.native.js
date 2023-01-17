bc49865e4969ba8de95131f5986dedaf03b89fd4
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-01-17 14:27:34 +0100

    Fix navigation error

--- Application/index.native.js
+++ Application/index.native.js
@@ -10,7 +10,7 @@ Navigation.events().registerAppLaunchedListener(() => {
         children: [
           {
             component: {
-              name: 'ConnexionScreen',
+              name: 'HomeScreen',
               options: {
                 topBar: {
                   visible: false
