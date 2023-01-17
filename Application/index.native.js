d80cbb53399cd739fec9ca126312e3a3e8cb541e
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-01-17 14:26:21 +0100

    Remove TopBar of react-native-navigation

--- Application/index.native.js
+++ Application/index.native.js
@@ -4,16 +4,22 @@ import App from "./src/App";
 Navigation.registerComponent('HomeScreen', () => App);
 Navigation.events().registerAppLaunchedListener(() => {
    Navigation.setRoot({
-     root: {
-       stack: {
-         children: [
-           {
-             component: {
-               name: 'HomeScreen'
-             }
-           }
-         ]
-       }
-     }
+    root: {
+      stack: {
+        id: 'mainStack',
+        children: [
+          {
+            component: {
+              name: 'ConnexionScreen',
+              options: {
+                topBar: {
+                  visible: false
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
   });
 });
