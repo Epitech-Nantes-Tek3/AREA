3e4fb11a79cee5fcb52d47f5c88bfcbe51223fd0
Cédric <cedric.corge@epitech.eu>
2023-01-17 15:41:18 +0100

    Get the weather forecast from api

--- Server/app.js
+++ Server/app.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 
 const path = require('path');
 const fs = require('fs');
+const {TwitterApi} = require('twitter-api-v2')
 
 const {port} = require('./config');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/weather', (req, res) => {
+    var request = http.get('http://api.open-meteo.com/v1/forecast?latitude=47.22&longitude=-1.55&hourly=rain', function (response) {
+        var buffer = ""
+        var data;
+
+        response.on("data", function (chunk) {
+            buffer += chunk;
+        }); 
+    
+
+        response.on("end", function (err) {
+            data = JSON.parse(buffer);
+            var date = new Date().toISOString().slice(0, 10);
+            var hour = new Date().toISOString().slice(11, 16);
+            console.log(hour)
+            for (var i = 0; i < data.hourly.time.length; i++) {
+                if (data.hourly.time[i].slice(0, 10) == date && data.hourly.time[i].slice(11, 16) == hour) {
+                    if (data.hourly.rain[i + 1] > 0) {
+                        console.log("Il pleut")
+                    }
+                    break;
+                }
+            }
+        })
+    })
+    res.send('Weather')
 })
 
 app.get('/about', (req, res) => {
