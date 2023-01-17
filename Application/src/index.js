e5aadc7b56747160ba810f351f1f6f5778e6c926
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-01-17 15:14:46 +0100

    Refactor Landing screen

--- Application/src/index.js
+++ Application/src/index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './Screens/ConnexionScreen';
  
 AppRegistry.registerComponent('App', () => App);
 AppRegistry.runApplication('App', {rootTag: document.getElementById('root')});
