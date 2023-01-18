074c455fb7fd697048d210f7ced2312ba97d8bdb
stom6 <tom.sarrazin@epitech.eu>
2023-01-18 18:01:56 +0100

    adding comments to the code

--- Server/Services/googleService.js
+++ Server/Services/googleService.js
@@ -6,10 +6,12 @@ const configgmail = require ('./../configGmail')
 const OAuth2_client = new OAuth2(configgmail.cliendId, configgmail.clientSecret)
 OAuth2_client.setCredentials( {refresh_token : configgmail.refreshToken})
 
+//receive a text. Sends it with the user mail ID of the configGmail.js to recipient of the configGmail.js .
 module.exports = {
     send_mail: function(mail_content) {
         const accessToken = OAuth2_client.getAccessToken()
 
+        // content of configGmail.js required by nodemailer
         const transport = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -22,13 +24,14 @@ module.exports = {
             }
         })
 
+        //content of the mail
         const mail_options = {
             from: `AREA BOT <${configgmail.user}>`,
             to: configgmail.recipient,
             subject: 'AREA poc test',
             text: get_html_message(mail_content)
         }
-
+        //send with nodemailer
         transport.sendMail(mail_options, function(error, res) {
             if (error) {
                 console.log('Error: ', error)
@@ -40,6 +43,8 @@ module.exports = {
         })
     }
 }
+
+// returns the text sent in mail format
 
 function get_html_message(mail_content) {
     return `${mail_content}`
--- Server/Services/openMeteoService.js
+++ Server/Services/openMeteoService.js
@@ -12,17 +12,22 @@ module.exports = {
             }); 
             response.on("end", function (err) {
                 data = JSON.parse(buffer);
+                // get date and time info
                 var date = new Date().toISOString().slice(0, 10);
                 var hour = new Date().getHours();
                 console.log(date, 'à', hour, 'heures')
+                // Search for data corresponding to the current date
                 for (var i = 0; i < data.hourly.time.length; i++) {
+                    // Compare date and time
                     if (data.hourly.time[i].slice(0, 10) == date && data.hourly.time[i].slice(11, 13) == hour) {
+                        // Check if it is raining
                         if (data.hourly.rain[i + 1] > 0) {
                             console.log(`Il pleut le ${date} à ${hour} heures`)
                             googleService.send_mail(`Il pleut le ${date} à ${hour} heures`)
+                        } else {
+                            console.log(`Il pleut pas le ${date} à ${hour} heures`)
+                            googleService.send_mail(`Il pleut pas le ${date} à ${hour} heures`)
                         }
-                        console.log(`Il pleut pas le ${date} à ${hour} heures`)
-                        googleService.send_mail(`Il pleut pas le ${date} à ${hour} heures`)
                         break;
                     }
                 }
