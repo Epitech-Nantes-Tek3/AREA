c653a24e63f049f2e6e170e060361f45eec72aa4
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-01-18 15:50:32 +0100

    Add second part of connexion screen
    Including social buttons and navigation button

--- Application/src/Screens/ConnexionScreen.tsx
+++ Application/src/Screens/ConnexionScreen.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, SafeAreaView, Image, Platform, Dimensions, TextInput, View, TouchableOpacity, ScaledSize } from "react-native";
-import Separator from "../Components/Separator";
+import Separator, { Line } from "../Components/Separator";
 import { Globals } from "../Common/Globals";
-import { AppleButton } from '../modules/react-native-apple-authentication';
+import FacebookSocialButton from "../Components/SocialButtons/FacebookButton";
+import GoogleSocialButton from "../Components/SocialButtons/GoogleSocialButton";
+import AppleSocialButton from "../Components/SocialButtons/AppleSocialButton";
+
 
 export default function ConnexionScreen() {
     // Sets different sizes to be good on both web and mobile
@@ -23,24 +26,56 @@ export default function ConnexionScreen() {
     }
 
     function connectionAction() {
-        console.log("Connect user")
+        console.log("Connect user", userMail, userPass)
     }
 
-    function onAppleButtonPress() {
+    function connectWithApple() {
         console.log("Connect with Apple")
+    }
+
+    function connectWithGoogle() {
+        console.log("Connect with Google")
+    }
+
+    function connectWithFacebook() {
+        console.log("Connect with Facebook")
+    }
+
+    function navigateToSubscribe() {
+        console.log("Navigate to subscribe")
     }
 
     function SocialButtons() {
         return (
             <View style={styles.socialContainer}>
-                {(Platform.OS === "ios" || Platform.OS === "macos") &&
-                    <AppleButton
-                        buttonStyle={AppleButton.Style.BLACK}
-                        buttonType={AppleButton.Type.CONTINUE}
-                        style={[styles.socialButtons, {width: percentageWidthInputs}]}
-                        onPress={onAppleButtonPress}
-                    />
-                }
+                <FacebookSocialButton onPress={connectWithFacebook} buttonViewStyle={[styles.socialButtons, {width: percentageWidthInputs}]} buttonText="Se connecter avec Facebook" />
+                <GoogleSocialButton onPress={connectWithGoogle} buttonViewStyle={[styles.socialButtons, {width: percentageWidthInputs}]} buttonText="Se connecter avec Google" />
+                {Platform.OS === "ios" &&
+                <AppleSocialButton onPress={connectWithApple} buttonViewStyle={[styles.socialButtons, {width: percentageWidthInputs}]}  buttonText="Se connecter avec Apple" />}
+            </View>
+        )
+    }
+
+    function SignUp() {
+        if (Platform.OS === "web") {
+            return (
+                <View style={signStyles.webContainer}>
+                    <Text style={signStyles.webNoAccountText}>Pas encore de compte ? </Text>
+                    <TouchableOpacity onPress={navigateToSubscribe} style={[styles.inputBorderStyle, styles.connectInside, {width: percentageWidthInputs}]}>
+                        <Text style={styles.connectText}>S'inscrire</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+        return(
+            <View style={signStyles.mobileContainer}>
+                <Line height={1} width={"60%"} borderWidth={0.5} borderColor={Globals.Colors.main} />
+                <View style={signStyles.textContainer}>
+                    <Text style={signStyles.mobileNoAccountText}>Pas encore de compte ? </Text>
+                    <TouchableOpacity onPress={navigateToSubscribe}>
+                        <Text style={signStyles.mobileSignInText}>S'inscrire</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -48,8 +83,8 @@ export default function ConnexionScreen() {
     function Circles() {
         return (
             <View>
-                <View style={{width: 406, height: 406, borderRadius: 500, backgroundColor: Globals.Colors.main, position: "absolute", top: -244, left: -55, opacity: 0.4}}/>
-                <View style={{width: 342, height: 342, borderRadius: 500, backgroundColor: Globals.Colors.main, position: "absolute", top: -203, left: 179, opacity: 0.4}}/>
+                <View style={circles.bigCircle}/>
+                <View style={circles.smallCircle}/>
             </View>
         )
     }
@@ -59,49 +94,54 @@ export default function ConnexionScreen() {
             {(Platform.OS === "ios" || Platform.OS == "android") &&
                 <Circles/>
             }
-            <Image
-                source={require("../assets/logo.png")}
-                style={
-                    [
-                        styles.image,
-                        {
-                            width: window.width / 100 * percentageWidthLogo,
-                            height: window.width / 100 * percentageWidthLogo
-                        }
-                    ]
-            }/>
-            <View style={[styles.inputContainer, {width: percentageWidthInputs}]}>
-                <TextInput
-                    style={[styles.inputBorderStyle, styles.inputInside]}
-                    onChangeText={(text) => setUserMail(text)}
-                    value={userMail}
-                    placeholder={"Email"}
-                    keyboardType="email-address"
-                    textContentType="emailAddress"
-                    autoComplete="email"
-                    autoCorrect={false}
-                    returnKeyType="next"
+            <View style={{flex: 10}}>
+                <Image
+                    source={require("../assets/logo.png")}
+                    style={
+                        [
+                            styles.image,
+                            {
+                                width: window.width / 100 * percentageWidthLogo,
+                                height: window.width / 100 * percentageWidthLogo
+                            }
+                        ]
+                }/>
+                <View style={[styles.inputContainer, {width: percentageWidthInputs}]}>
+                    <TextInput
+                        style={[styles.inputBorderStyle, styles.inputInside]}
+                        onChangeText={(text) => setUserMail(text)}
+                        value={userMail}
+                        placeholder={"Email"}
+                        placeholderTextColor={"#7B7B7B"}
+                        keyboardType="email-address"
+                        textContentType="emailAddress"
+                        autoComplete="email"
+                        autoCorrect={false}
+                        returnKeyType="next"
 
-                />
-                <TextInput
-                    style={[styles.inputBorderStyle, styles.inputInside]}
-                    onChangeText={(text) => setUserPass(text)}
-                    value={userPass}
-                    placeholder="Mot de passe"
-                    secureTextEntry={true}
-                    autoComplete="password"
-                    autoCorrect={false}
-                    returnKeyType="done"
-                />
-                <Text style={styles.forgottenText} onPress={forgotPassword}>Un oubli ?</Text>
-                <TouchableOpacity
-                    style={[styles.inputBorderStyle, styles.connectInside]}
-                    onPress={connectionAction}>
-                    <Text>Se connecter</Text>
-                </TouchableOpacity>
+                    />
+                    <TextInput
+                        style={[styles.inputBorderStyle, styles.inputInside]}
+                        onChangeText={(text) => setUserPass(text)}
+                        value={userPass}
+                        placeholder="Mot de passe"
+                        placeholderTextColor={"#7B7B7B"}
+                        secureTextEntry={true}
+                        autoComplete="password"
+                        autoCorrect={false}
+                        returnKeyType="done"
+                    />
+                    <Text style={styles.forgottenText} onPress={forgotPassword}>Un oubli ?</Text>
+                    <TouchableOpacity
+                        style={[styles.inputBorderStyle, styles.connectInside]}
+                        onPress={connectionAction}>
+                        <Text style={styles.connectText}>Se connecter</Text>
+                    </TouchableOpacity>
+                </View>
+                <Separator width={percentageWidthSeparator} text="ou"/>
+                <SocialButtons/>
             </View>
-            <Separator width={percentageWidthSeparator} text="ou"/>
-            <SocialButtons/>
+            <SignUp/>
         </SafeAreaView>
     )
 }
@@ -117,7 +157,7 @@ const styles = StyleSheet.create({
         marginTop: 41
     },
     inputContainer: {
-        marginTop: 70,
+        marginTop: 40,
         alignSelf: "center"
     },
     inputBorderStyle: {
@@ -136,7 +176,8 @@ const styles = StyleSheet.create({
         paddingRight: 5,
         color: Globals.Colors.dark,
         marginBottom: 8,
-        fontSize: 12
+        fontSize: 12,
+        fontFamily: "Poppins-Medium"
     },
     connectInside: {
         borderColor: Globals.Colors.main,
@@ -149,6 +190,74 @@ const styles = StyleSheet.create({
     },
     socialButtons: {
         height: 50,
-        marginBottom: 16,
+        borderRadius: 10,
+    },
+    bottomLine: {
+        alignSelf: "center"
+    },
+    connectText: {
+        fontSize: 16,
+        fontFamily: "Poppins-Medium"
+    }
+})
+
+const signStyles = StyleSheet.create({
+    webContainer: {
+        alignSelf: "center",
+        flexDirection: "column",
+        marginTop: 25,
+        alignItems: "center",
+        width: "100%"
+    },
+    webNoAccountText: {
+        color: "#6E8387",
+        fontFamily: "Poppins-Medium",
+        fontSize: 20
+    },
+    mobileContainer: {
+        flex:1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    textContainer: {
+        alignSelf: "center",
+        flexDirection: "row",
+        marginTop: 25,
+        alignItems: "center",
+        width: "100%",
+        justifyContent: "center"
+    },
+    mobileNoAccountText: {
+        color: "#6E8387",
+        fontFamily: "Poppins-Medium",
+        fontSize: 18
+    },
+    mobileSignInText: {
+        color: "#95B8D1",
+        fontFamily: "Poppins-Bold",
+        fontSize: 18
+    }
+})
+
+const circles = StyleSheet.create({
+    bigCircle: {
+        width: 406,
+        height: 406,
+        borderRadius: 500,
+        backgroundColor: Globals.Colors.main,
+        position: "absolute",
+        top: -244,
+        left: -55,
+        opacity: 0.4
+    },
+    smallCircle: {
+        width: 342,
+        height: 342,
+        borderRadius: 500,
+        backgroundColor: Globals.Colors.main,
+        position: "absolute",
+        top: -203,
+        left: 179,
+        opacity: 0.4
     }
 })
