ef2716ceb09b3b1d81d3478bb505382bb5f50277
Cédric <cedric.corge@epitech.eu>
2023-01-20 14:55:13 +0100

    Add connection with firebase and register route

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express();
-const {port} = require('./config');
+const config = require('./config');
+var bodyParser = require('body-parser')
+
+const port = config.port;
+
+const firebase = require("firebase");
+
+const firebaseConfig = require('./firebaseConfig');
+
+firebase.initializeApp(firebaseConfig.api);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  firebase.auth()
+  .createUserWithEmailAndPassword(email, password).then((userCredential) => {
+    console.log('Successfully created new user:', userCredential.user.uid)
+    res.sendStatus(200);
+  })
+  .catch((error) => {
+    console.log('Error creating new user:', error);
+    res.send(error).status(400);
+  });
+})
+
+/*app.post('/login', (req, res) => {
+  const {email, password} = req.body;
+  firebase.auth().signInWithEmailAndPassword(email, password).then((user) => {
+    console.log(user)
+    res.sendStatus(200)
+  });
+})*/
 
 app.listen(port, () => {
     console.log(`AREA app server listening on port ${port}!`)
--- Server/config.js
+++ Server/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = {
-    port: process.env.PORT || 8080
-};
+const port = process.env.PORT || 8080;
+
+exports.port = port
