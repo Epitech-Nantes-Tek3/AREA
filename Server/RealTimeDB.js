4ac1d69faf6092d913ffe038dd2767867f5256bc
stom6 <tom.sarrazin@epitech.eu>
2023-01-20 13:08:55 +0100

    addition of firebaseconfig required to connect to the db.

--- Server/RealTimeDB.js
+++ Server/RealTimeDB.js
@@ -1,16 +1,7 @@
 const firebase = require("firebase");
 
 // Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyAKc_tJD5cFrUnWwD9sIfxl7Xo1Q5NWarM",
-    authDomain: "area-f3463.firebaseapp.com",
-    databaseURL: "https://area-f3463-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "area-f3463",
-    storageBucket: "area-f3463.appspot.com",
-    messagingSenderId: "1081092664358",
-    appId: "1:1081092664358:web:e57f97ac635b04de3d0ae7",
-    measurementId: "G-1JNT4XPRZN"
-  };
+const firebaseConfig = require('./firebaseconfig')
 firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the database
