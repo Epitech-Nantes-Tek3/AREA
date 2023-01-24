418f51f7e21da9031b678d93e6a0bd623866d06b
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-01-24 17:27:34 +0100

    Register AddArea screen

--- Application/index.js
+++ Application/index.js
@@ -1,4 +1,5 @@
 import { Navigation } from "react-native-navigation";
+import AddArea from "./src/Screens/AddArea";
 import ConnexionScreen from "./src/Screens/ConnexionScreen";
 import HomeScreen from "./src/Screens/HomeScreen";
 import SignInScreen from "./src/Screens/SignInScreen";
@@ -6,6 +7,7 @@ import SignInScreen from "./src/Screens/SignInScreen";
 Navigation.registerComponent('ConnexionScreen', () => ConnexionScreen);
 Navigation.registerComponent('SignInScreen', () => SignInScreen);
 Navigation.registerComponent('HomeScreen', () => HomeScreen);
+Navigation.registerComponent('AddArea', () => AddArea);
 Navigation.events().registerAppLaunchedListener(() => {
    Navigation.setRoot({
     root: {
