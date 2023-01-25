b3fd93c8bd69da8cbda970e2bcbb21f7322fe341
Nathan <nathan.rousseau@epitech.eu>
2023-01-25 16:04:30 +0100

    Get the ISS Data (longitude and latitude) #42

--- Server/Services/ISSStationService.js
+++ Server/Services/ISSStationService.js
@@ -4,11 +4,20 @@ const url = 'http://api.open-notify.org/iss-now.json';
 module.exports = {
     checkISSPosition: function(res) {
         http.get(url, function (response) {
+            var buffer = ""
+            
             response.on("data", function (chunk) {
-                console.log("J'AI DATA")
+                buffer += chunk
             });
+
+            response.on("end", function (err) {
+                var data = JSON.parse(buffer)
+                var longitude = data.iss_position.longitude
+                var latitude = data.iss_position.latitude
+            });
+
             response.on('error', function (err) {
-                console.log('Problème 1')
+                console.log('Error while getting ISS Data')
             });
 
         })
