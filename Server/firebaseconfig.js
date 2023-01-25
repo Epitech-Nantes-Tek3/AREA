369dcdf2184bc5502e60330fb5c64dd9094a41cf
osvegn <thomas.prudhomme@epitech.eu>
2023-01-25 14:19:32 +0100

    Remove bad pushed file, remove useless comment #12

--- Server/firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyA7O-BdSKuj-IfR4S7HSEJCXMy4yns7GGU",
-    authDomain: "area-official.firebaseapp.com",
-    databaseURL: "https://area-official-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "area-official",
-    storageBucket: "area-official.appspot.com",
-    messagingSenderId: "542173031512",
-    appId: "1:542173031512:web:8e6f26885edb584c46ae2d"
-}
