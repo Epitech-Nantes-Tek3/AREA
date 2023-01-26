3496873cf73687e73b9b76ac782008c99f926d72
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-26 01:20:13 +0000

    Change output for summary

--- action/index.js
+++ action/index.js
@@ -29,7 +29,7 @@ for (var i in files) {
 await core.summary
   .addHeading('Test Results')
   .addTable([
-    [{data: 'File', header: true}, files],
-    [{data: 'Result', header: true}, result]
+    [{data: 'File', header: true}, ].concat(files),
+    [{data: 'Result', header: true}].concat(result)
   ])
   .write()
