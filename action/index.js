badfc0ceb35568ee7e2ca0a049f5a6e21b55ecf0
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-26 00:54:07 +0000

    Try to write on github summary #34

--- action/index.js
+++ action/index.js
@@ -13,3 +13,15 @@ try {
 } catch (error) {
   core.setFailed(error.message);
 }
+
+await core.summary
+  .addHeading('Test Results')
+  .addCodeBlock(generateTestResults(), "js")
+  .addTable([
+    [{data: 'File', header: true}, {data: 'Result', header: true}],
+    ['foo.js', 'Pass '],
+    ['bar.js', 'Fail '],
+    ['test.js', 'Pass ']
+  ])
+  .addLink('View staging deployment!', 'https://github.com')
+  .write()
