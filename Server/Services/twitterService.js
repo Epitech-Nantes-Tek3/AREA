075ba7ccfc4e4de24703526bd7cd608ae8aa531c
stom6 <tom.sarrazin@epitech.eu>
2023-01-28 00:00:36 +0100

    added comments for the twitterService file

--- Server/Services/twitterService.js
+++ Server/Services/twitterService.js
@@ -2,6 +2,22 @@ const firebaseFunctions = require('../firebaseFunctions')
 const LoginWithTwitter = require('login-with-twitter');
 const { TwitterApi } = require('twitter-api-v2')
 
+/**
+ * @brief uses the Twitter API to perform different actions depending on the action argument. Depending on 
+ * the value of action, the function can send a tweet, search for tweets and like or retweet them.
+ * @param {*} req is an object that contains information about the HTTP request that called this function
+ * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+ * @param {*} appKey is an application key provided by Twitter to access their API. It is used to identify the 
+ * application making the request.
+ * @param {*} appSecret is an application secret provided by Twitter to access their API. It is used to secure 
+ * the requests made by the application.
+ * @param {*} bearer is an access token that is used to access certain parts of the Twitter API that require
+ *  additional authorization.
+ * @param {*} hashtagOrMessage is an argument that can be a message or a hashtag depending on the value of the 
+ * action argument. It is used to send a tweet or to search for tweets.
+ * @param {*} action is an argument that determines what action should be performed by the function. It 
+ * can take the values 'tweet', 'like' or 'retweet'.
+ */
 async function carryOutAnAction(req, res, appKey, appSecret, bearer, hashtagOrMessage, action) {
     const client = new TwitterApi ({
         appKey: appKey,
@@ -36,6 +52,13 @@ async function carryOutAnAction(req, res, appKey, appSecret, bearer, hashtagOrMe
 }
 
 module.exports = {
+    /**
+     * @brief uses an external "LoginWithTwitter" package to connect a user to their Twitter account.retrieves the 
+     * application key and application secret information from Firebase and uses it for "tw" of "LoginWithTwitter". 
+     * Then, it uses the "login" function of "tw" to redirect the user to a Twitter authorization URL.
+    * @param {*} req is an object that contains information about the HTTP request that called this function
+    * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+     */
     loginTwitter: function(req, res) {
         firebaseFunctions.getDataFromFireBase('twitter', '')
         .then(data => {
@@ -59,6 +82,12 @@ module.exports = {
             console.log(error);
         });
     },
+    /**
+     * @brief makes sure the user is logged in by checking if "req.session.user" exists. If so, it renders 
+     * the "dash" view to display the dash page.
+     * @param {*} req is an object that contains information about the HTTP request that called this function
+     * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+     */
     dashTwitter: function(req, res) {
         if (req.session.user) {
             res.render('dash')
@@ -66,6 +95,13 @@ module.exports = {
             res.redirect('/twitter')
         }
     },
+    /**
+     * @brief is used to process Twitter information after the user has logged into their account. It retrieves the 
+     * parameters the tokens from the user's request, and those from the api on firebase. Use "tw" for the "callback" 
+     * function. If this function succeeds, it stores the user's information in the session, then redirects dash.
+     * @param {*} req is an object that contains information about the HTTP request that called this function
+     * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+     */
     signTwitter: function(req, res) {
         var params = {
             oauth_token: req.query.oauth_token,
@@ -88,6 +124,12 @@ module.exports = {
             console.log(error);
         });
     },
+    /**
+     * @brief allows the user to post a tweet. It first checks if there is a "req.session.user". 
+     * Then it uses the "carryOutAnAction" function to post a tweet with the message content.
+     * @param {*} req is an object that contains information about the HTTP request that called this function
+     * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+     */
     sendTweet: function(req, res) {
         if (req.session.user) {
             firebaseFunctions.getDataFromFireBase('twitter', '')
@@ -101,6 +143,12 @@ module.exports = {
             res.redirect('/twitter')
         }
     },
+    /**
+     * @brief allows the user to retweet the most recent tweet of the hashtag pass parameter. It first checks 
+     * if user exist then it uses  "carryOutAnAction" function to retweet newest tweet of the hashtag.
+     * @param {*} req is an object that contains information about the HTTP request that called this function
+     * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+     */
     putlike: function(req, res) {
         if (req.session.user) {
             firebaseFunctions.getDataFromFireBase('twitter', '')
@@ -114,6 +162,12 @@ module.exports = {
             res.redirect('/twitter')
         }
     },
+    /**
+     * @brief allows the user to like the most recent tweet of the hashtag pass parameter. It first checks 
+     * if user exist then it uses  "carryOutAnAction" function to like newest tweet of the hashtag.
+     * @param {*} req is an object that contains information about the HTTP request that called this function
+     * @param {*} res is an object that handles the HTTP response that will be sent to the user.
+     */
     putRetweet: function(req, res) {
         if (req.session.user) {
             firebaseFunctions.getDataFromFireBase('twitter', '')
