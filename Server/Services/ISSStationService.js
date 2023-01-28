87e3397dc8a4db0b838a8150c9f72c035453b697
Nathan <nathan.rousseau@epitech.eu>
2023-01-28 15:01:07 +0100

    Distance between User and ISS Station Done ! #42

--- Server/Services/ISSStationService.js
+++ Server/Services/ISSStationService.js
@@ -1,7 +1,9 @@
+const { cp } = require('fs');
 const http  = require('http');
 const firebaseFunctions = require('../firebaseFunctions');
 const url = 'http://api.open-notify.org/iss-now.json';
-const gap = 100
+const gap = 1000.0
+const heartRadius = 6374
 
 module.exports = {
     checkISSPosition: function(res, uid) {
@@ -17,12 +19,20 @@ module.exports = {
 
                 response.on("end", function (err) {
                     var dataISS = JSON.parse(buffer)
-                    var longitudeISS = dataISS.iss_position.longitude
-                    var latitudeISS = dataISS.iss_position.latitude
-                    var longitudeUser = data.longitude
-                    var latitudeUser = data.latitude
+                    var longitudeISS = dataISS.iss_position.longitude //B2
+                    var latitudeISS = dataISS.iss_position.latitude //C2
+                    var longitudeUser = data.longitude //B3
+                    var latitudeUser = data.latitude //
 
-                    if (true)
+                    console.log(longitudeISS, latitudeISS, longitudeUser, latitudeUser)
+
+                    /// https://www.01net.com/actualites/calculer-la-distance-entre-2-points-sur-terre-522697.html#:~:text=La%20formule%20(%C3%A0%20saisir%20par,C3)))*6371.
+                    var distance = Math.acos(Math.sin(Radiant(longitudeISS)) * Math.sin(Radiant(longitudeUser)) +
+                        Math.cos(Radiant(longitudeISS)) * Math.cos(Radiant(longitudeUser)) * Math.cos(Radiant(latitudeISS - latitudeUser))) * heartRadius
+
+                    console.log(distance)
+
+                    if (distance <= gap)
                         InfoISS = "THE ISS IS NEAR US BEWARE"
                     else
                         InfoISS =  "THE ISS IS FAR IT'S FINE"
@@ -39,4 +49,12 @@ module.exports = {
             console.log(error);
         });
     }
+}
+
+/// Convert a degree value in radiant
+// - degrees : value to be converted
+function Radiant(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
 }
