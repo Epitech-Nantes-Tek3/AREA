e7b459a1b47a952b4cc5b5fdc4b214f3088046f3
Nathan <nathan.rousseau@epitech.eu>
2023-01-30 22:49:27 +0100

    Move MainPage in MainPage.js #59

---
+++ Web/src/MainPage.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+/**
+ * @brief Return the home page for AREA
+ * TO UPDATE
+ */
+function MainPage() {
+    return (
+        <div className="App">
+      <header className="App-header">
+        <b> Welcome </b>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    );
+}
+
+export default MainPage;
