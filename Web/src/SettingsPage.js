70f507a0379b29ef594e4daee7d41807f1431e48
osvegn <thomas.prudhomme@epitech.eu>
2023-02-11 13:00:42 +0100

    Add "retour" button in settings page and use image for deconnexion button

--- Web/src/SettingsPage.js
+++ Web/src/SettingsPage.js
@@ -8,6 +8,7 @@ import SpotifyImage from './assets/spotify.png';
 import TwitterImage from './assets/twitter.png';
 import TwitchImage from './assets/twitch.png';
 import StravaImage from './assets/strava.png';
+import DeconnexionImage from './assets/deconnexion.png';
 import { getAllCacheData, addDataIntoCache } from './CacheManagement'
 
 export default function SettingsPage(props) {
@@ -87,64 +88,105 @@ export default function SettingsPage(props) {
     return (
         <div id='global'>
             <div id='home'>
-                <img
-                    src={HomeImage}
-                    alt="Home"
-                    style={{
-                    width:50,
-                    height:50
-                    }}
-                    onClick={() => {
-                        navigate('/home')
-                    }}
-                >
-                </img>
+                <div id='homehome' class='clickable-text'>
+                    <img
+                        src={HomeImage}
+                        alt="Home"
+                        style={{
+                            width:50,
+                            height:50
+                        }}
+                        onClick={() => {
+                            navigate('/home')
+                        }}
+                    >
+                    </img>
+                    <p>Retour</p>
+                </div>
             </div>
             <h1>Settings</h1>
             <p>Mail: {props.userInformation.mail}</p>
             <p>Location: {(props.userInformation.coord.city == null) ? props.userInformation.coord.city : "UNDEFINED"}</p>
-            <img src={GoogleImage}
-                alt="Google"
-                style={{
-                    width:50,
-                    height:50
-                }}
-            ></img><br></br>
-            <img src={SpotifyImage}
-                alt="Spotify"
-                style={{
-                    width:50,
-                    height:50
-                }}
-            ></img><br></br>
-            <img src={TwitterImage}
-                alt="Twitter"
-                style={{
-                    width:50,
-                    height:50
-                }}
-            ></img><br></br>
-            <img src={StravaImage}
-                alt="Strava"
-                style={{
-                    width:50,
-                    height:50
-                }}
-            ></img><br></br>
-            <img src={TwitchImage}
-                alt="Twitch"
-                style={{
-                    width:50,
-                    height:50
-                }}
-            ></img><br></br>
-            <p
-                id='deconnexion'
-                onClick={() => {
-                    addDataIntoCache("area", {ip}, {});
-                    navigate('/auth')
-                }}
-            >Déconnexion</p>
+            <div id="connexion-services">
+                <div
+                class='clickable-text'
+                >
+                    <img src={GoogleImage}
+                        alt="Google"
+                        style={{
+                            width:50,
+                            height:50
+                        }}
+                    ></img>
+                    <p>Connexion à google</p>
+                </div>
+                <div
+                class='clickable-text'
+                >
+                    <img src={SpotifyImage}
+                        alt="Spotify"
+                        style={{
+                            width:50,
+                            height:50
+                        }}
+                    ></img>
+                    <p>Connexion à spotify</p>
+                </div>
+                <div
+                class='clickable-text'
+                >
+                    <img src={TwitterImage}
+                        alt="Twitter"
+                        style={{
+                            width:50,
+                            height:50
+                        }}
+                    ></img>
+                    <p>Connexion à twitter</p>
+                </div>
+                <div
+                class='clickable-text'
+                >
+                    <img src={StravaImage}
+                        alt="Strava"
+                        style={{
+                            width:50,
+                            height:50
+                        }}
+                    ></img>
+                    <p>Connexion à strava</p>
+                </div>
+                <div
+                class='clickable-text'
+                >
+                    <img src={TwitchImage}
+                        alt="Twitch"
+                        style={{
+                            width:50,
+                            height:50
+                        }}
+                    ></img>
+                    <p>Connexion à twitch</p>
+                </div>
+            </div>
+            <div
+                id='deconnexion'>
+                <div
+                    class='clickable-text'
+                    style={{
+                        width:100,
+                    }}
+                    onClick={() => {
+                        addDataIntoCache("area", {ip}, {});
+                        navigate('/auth')
+                    }}
+                >
+                    <img
+                        src={DeconnexionImage}
+                    ></img>
+                    <p>Deconnexion</p>
+                </div>
+            </div>
         </div>
     )
 }
