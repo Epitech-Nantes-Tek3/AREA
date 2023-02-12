b013f4755d1ba204388f8ba2f190fcee44fd9b69
osvegn <thomas.prudhomme@epitech.eu>
2023-02-12 23:20:46 +0100

    Improve docker-compose-consistancy script with yq

--- .github/scripts/docker-compose-consistancy.sh
+++ .github/scripts/docker-compose-consistancy.sh
@@ -7,8 +7,8 @@
 # Author: Thomas
 ###
 
-function lineExists {
-    grep $1 docker-compose.yml > /dev/null
+function findElement {
+    yq e $1 docker-compose.yml > /dev/null
     if (( $? != 0 ))
     then
         echo "| $1 | NOT FOUND |"
@@ -21,33 +21,60 @@ function lineExists {
 
 nbTests=0
 nbSuccess=0
+exitCode=0
 
 echo "| Files | Results |"
 echo "|-------|---------|"
 
-lineExists "services:"
-nbSuccess=$(( $nbSuccess + $?))
-nbTests=$(( $nbTests + 1 ))
-lineExists "server:"
-nbSuccess=$(( $nbSuccess + $?))
-nbTests=$(( $nbTests + 1 ))
-lineExists "client_web:"
-nbSuccess=$(( $nbSuccess + $?))
-nbTests=$(( $nbTests + 1 ))
-lineExists "client_mobile:"
-nbSuccess=$(( $nbSuccess + $?))
-nbTests=$(( $nbTests + 1 ))
-lineExists "depends_on:"
-nbSuccess=$(( $nbSuccess + $?))
-nbTests=$(( $nbTests + 1 ))
+elementToFind=(".services.server"
+".services.client_web"
+".services.client_mobile"
+".services.client_web.depends_on"
+".services.client_web.depends_on.server"
+".services.client_mobile.depends_on"
+".services.client_mobile.depends_on.server"
+".services.client_web.depends_on.client_mobile")
+
+for element in "${elementToFind[@]}"
+do
+    findElement $element
+    nbSuccess=$(( $nbSuccess + $?))
+    nbTests=$(( $nbTests + 1 ))
+done
 
 echo ""
 
 if (( $nbSuccess == $nbTests ))
 then
     echo "All files are found ($nbTests)."
-    exit 0
 else
     echo "Some files are missing ($(( $nbTests - $nbSuccess )))"
-    exit 1
+    exitCode=1
 fi
+
+diff <(yq e '.services.client_web.volumes' docker-compose.yml) <(yq e '.services.client_mobile.volumes' docker-compose.yml) > /dev/null
+if (( $? != 0 ))
+then
+    echo "Client_web and Client_mobile volumes are not the same"
+    exitCode=1
+else
+    echo "Client_web and Client_mobile volumes are the same"
+fi
+
+diff <(yq e '.services.server.ports' docker-compose.yml) <(echo "- \"8080:8080\"") > /dev/null
+if (( $? != 0 ))
+then
+    echo "Server ports are not the same"
+    exitCode=1
+else
+    echo "Server ports are the same"
+fi
+diff <(yq e '.services.client_web.ports' docker-compose.yml) <(echo "- \"8081:3000\"") > /dev/null
+if (( $? != 0 ))
+then
+    echo "Client_web ports are not the same"
+    exitCode=1
+else
+    echo "Client_web ports are the same"
+fi
+exit $exitCode
