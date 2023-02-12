ef7e168915f834d25c4f1adb4f33d9690fd9fc82
Nathan <nathan.rousseau@epitech.eu>
2023-02-12 13:17:42 +0100

    Add First Login to Spotify #114

--- Server/app.js
+++ Server/app.js
@@ -246,4 +246,36 @@ app.get('/getPosition/:uid', (req, res) => {
             console.log(error);
             res.json(error).status(400);
         })
+
+})
+
+app.get('/spotify', (req, res) => {
+    var SpotifyWebApi = require('spotify-web-api-node');
+
+    var spotifyApi = new SpotifyWebApi({
+        clientId: 'ebf465caf20c4323aeb1537915aa645c',
+        clientSecret: '2f0c839715874985809a29db5cc0686f'
+      });
+
+    spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+            function(data) {
+              console.log('Artist albums', data.body);
+            },
+            function(err) {
+              console.error(err);
+            }
+          );
+    },
+    function(err) {
+        console.log('Something went wrong when retrieving an access token', err);
+    }
+    );
+
 })
