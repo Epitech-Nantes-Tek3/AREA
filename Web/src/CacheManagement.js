82054f7d74cdd2b9ecf4987f3d02b56f415d36ac
osvegn <thomas.prudhomme@epitech.eu>
2023-02-13 15:29:36 +0100

    Add missing documentation in CacheManagement

--- Web/src/CacheManagement.js
+++ Web/src/CacheManagement.js
@@ -1,5 +1,9 @@
 import { ip } from './env'
 
+/**
+ * This function gets all the data from the cache and returns it
+ * @returns the cacheData variable.
+ */
 export async function getAllCacheData() {
     var cacheData;
     var url = {ip}
@@ -16,6 +20,13 @@ export async function getAllCacheData() {
     return cacheData;
 };
 
+/**
+ * It takes a cache name, a URL, and a response, and then it adds the response to
+ * the cache
+ * @param cacheName - The name of the cache to store the data in.
+ * @param url - The URL of the data to be cached.
+ * @param response - The response object that you want to cache.
+ */
 export function addDataIntoCache (cacheName, url, response) {
     const data = new Response(JSON.stringify(response));
     if ('caches' in window) {
