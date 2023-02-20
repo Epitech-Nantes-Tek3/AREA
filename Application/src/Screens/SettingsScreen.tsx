6d750fdfd619706e1898e7242650cbe970235328
Nathan <nathan.rousseau@epitech.eu>
2023-02-20 13:57:19 +0100

    Try to add spotify Auth on Mobile #144

--- Application/src/Screens/SettingsScreen.tsx
+++ Application/src/Screens/SettingsScreen.tsx
@@ -4,6 +4,9 @@ import { Globals } from "../Common/Globals";
 import Geolocation from 'react-native-geolocation-service';
 import { SettingsProps } from "../Common/Interfaces";
 
+import { stringify } from 'querystring';
+import { locationURL } from 'location-href';
+
 interface Location {
     latitude: number
     longitude: number
@@ -96,7 +99,7 @@ export default function SettingsScreen(props: SettingsProps) {
                 },
                 { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
             )
-        } 
+        }
     }
 
     function ProfileInfo() {
@@ -189,7 +192,22 @@ export default function SettingsScreen(props: SettingsProps) {
             })
         }
 
-        function spotifyConnexion() {
+         /**
+         * Generates a random string containing numbers and letters
+         * @param  {number} length The length of the string
+         * @return {string} The generated string
+         */
+        var generateRandomString = function(length : number) {
+            var text = '';
+            var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+            for (var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
+        };
+
+        async function spotifyConnexion() {
             let token = "ImTestingATokenItIsSoFunnySpotify"
             props.setUserInfo({
                 mail: props.userInfo.mail,
@@ -208,6 +226,52 @@ export default function SettingsScreen(props: SettingsProps) {
                 },
                 ip: props.userInfo.ip
             })
+            const scopes = [
+                'ugc-image-upload',
+                'user-read-playback-state',
+                'user-modify-playback-state',
+                'user-read-currently-playing',
+                'streaming',
+                'app-remote-control',
+                'user-read-email',
+                'user-read-private',
+                'playlist-read-collaborative',
+                'playlist-modify-public',
+                'playlist-read-private',
+                'playlist-modify-private',
+                'user-library-modify',
+                'user-library-read',
+                'user-top-read',
+                'user-read-playback-position',
+                'user-read-recently-played',
+                'user-follow-read',
+                'user-follow-modify'
+              ];
+
+            try {
+                await fetch(ip + "/spotify").then(response => {
+                    response.json().then(data => {
+                        var clientID = data
+
+                        const url = 'https://accounts.spotify.com/authorize?' +
+                            stringify({
+                            response_type: 'code',
+                            client_id: clientID,
+                            scope: scopes,
+                            show_dialog : true,
+                            redirect_uri: 'http://localhost:8080/spotify/callback',
+                            state : generateRandomString(16)
+                        })
+
+                        locationURL.set(url)
+                    })
+                }).catch(error => {
+                    console.log(error)
+                })
+            } catch (error) {
+                console.log(error);
+            }
+
         }
 
         function twitterConnexion() {
