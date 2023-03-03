288d33a4ec12cfeefbce3e11ddeb1750e9106388
osvegn <thomas.prudhomme@epitech.eu>
2023-03-03 09:31:49 +0100

    Redirect to login page after register

--- Web/src/AuthPage.js
+++ Web/src/AuthPage.js
@@ -119,8 +119,13 @@ function AuthPage(props) {
                         setIsBadPassword(false);
                         props.userInformation.id = data.userUid;
                         props.userInformation.mail = email;
-                        addDataIntoCache("area", { mail: props.userInformation.mail, id: props.userInformation.id, password: btoa(JSON.parse(requestOptions.body).password), ip: props.userInformation.ip });
-                        navigate('/home');
+                        if (endpoint === '/register') {
+                            alert("Vous êtes bien inscrit ! Vérifier votre boite mail pour confirmer votre inscription. Vous pouvez maintenant vous connecter.")
+                            window.location.reload()
+                        } else {
+                            addDataIntoCache("area", { mail: props.userInformation.mail, id: props.userInformation.id, password: btoa(JSON.parse(requestOptions.body).password), ip: props.userInformation.ip });
+                            navigate('/home');
+                        }
                     } else {
                         setIsBadPassword(true);
                     }
